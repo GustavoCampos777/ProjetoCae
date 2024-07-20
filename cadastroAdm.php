2dd6c7895f77e9a025f38936fa7d9fdc2af692f2
unknown <gustavo124moreira@gmail.com>
2024-07-20 03:30:06 -0300

    cadastro adm e estudante

---
+++ cadastroAdm.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <title> Cadastro </title>
+</head>
+
+<body>
+    <h1>Cadastro do Sistema</h1>
+    <form method="post" action="validarCadastroAdm.php">
+        <table>
+
+            <tr>
+                <td>Cpf:</td>
+                <td><input name="user" type="text"></td>
+            </tr>
+            <tr>
+                <td>Nome:</td>
+                <td><input name="senha" type="password"></td>
+            </tr>
+            <tr>
+                <td>função:</td>
+                <td><input name="senha" type="password"></td>
+            </tr>
+            <tr>
+                <td>Senha:</td>
+                <td><input name="senha" type="password"></td>
+            </tr>
+        </table>
+        <br>
+        <button type="submit" name="enviar"> Cadastrar </button>
+    </form>
+</body>
+
+</html>
---
+++ cadastroEstudante.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+    <title> Cadastro de Estudante </title>
+</head>
+
+<body>
+    <h1>Cadastro de Estudante</h1>
+    <form method="post" action="validarCadastroEstudante.php">
+        <table>
+
+            <tr>
+                <td>Matricula:</td>
+                <td><input name="user" type="text"></td>
+            </tr>
+            <tr>
+                <td>Nome:</td>
+                <td><input name="user" type="text"></td>
+            </tr>
+            <tr>
+                <td>Contato aluno:</td>
+                <td><input name="user" type="text"></td>
+            </tr>
+            <tr>
+                <td>Contato Responsavel:</td>
+                <td><input name="user" type="text"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input name="user" type="text"></td>
+            </tr>
+        </table>
+        <br>
+        <button type="submit" name="enviar"> Cadastrar </button>
+    </form>
+</body>
+
+</html>
